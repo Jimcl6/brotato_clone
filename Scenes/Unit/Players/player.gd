c9397466f6b9edf111a5e7a54e2de33a6c8ece75
Jimcl6 <jedllorente@gmail.com>
2026-04-27 23:09:29 +0800

    commit: con't implementing player dash for player

--- Scenes/Unit/Players/player.gd
+++ Scenes/Unit/Players/player.gd
@@ -1,7 +1,21 @@
 extends Unit
 class_name Player
 
-var move_dir : Vector2
+@export var dash_duration:= 0.5
+@export var dash_speed_multi:= 0.5
+@export var dash_cooldown:= 0.5
+
+@onready var dash_timer: Timer = $DashTimer
+@onready var dash_cool_down_timer: Timer = $DashCoolDownTimer
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+var move_dir: Vector2
+var is_dashing := false
+var dash_available := true
+
+func _ready() -> void:
+	dash_timer.wait_time = dash_duration
+	dash_cool_down_timer.wait_time = dash_cooldown
 
 func _process(delta: float) -> void:
 	move_dir = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -26,3 +40,7 @@ func update_rotation() -> void:
 		visuals.scale = Vector2(-0.5,0.5)
 	else:
 		visuals.scale = Vector2(0.5,0.5)
+		
+func start_dash() -> void:
+	is_dashing = true
+	dash_timer.start()
