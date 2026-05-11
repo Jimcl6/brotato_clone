1f21b8c53146f0c958c3cca8a7ba7bd23cd5888b
Jimcl6 <jedllorente@gmail.com>
2026-05-11 20:55:02 +0800

    commit: commit implemented flash upon damage by enemy feature
    Signed-off-by: Jimcl6 <jedllorente@gmail.com>

--- autoloads/global.gd
+++ autoloads/global.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var player: Player
+const FLASH_MATERIAL = preload("uid://bfv8fopedp2de")
--- hurt_box_component.gd
+++ hurt_box_component.gd
@@ -6,4 +6,3 @@ signal on_damaged(hitBox:HitboxComponent )
 func _on_area_entered(area: Area2D) -> void:
 	if area is HitboxComponent:
 		on_damaged.emit(area)
-		print(area.owner.name)
